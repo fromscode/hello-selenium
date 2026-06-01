90d70cae009f26a358c9205c6f4a1b12e2d42aad
fromscode <chakrabortyshubham00@gmail.com>
2026-06-01 11:34:26 +0530

    Find Web Elements

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,22 +1,35 @@
 package com.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
         WebDriver driver = new ChromeDriver();
-        driver.get("http://opencart.com");
 
-        String title = driver.getTitle();
+        try {
+            driver.get("http://opencart.com");
 
-        if (title.contains("OpenCart")) {
-            System.out.println("Test passed");
+            String title = driver.getTitle();
+
+            assert title != null;
+            if (title.contains("OpenCart")) {
+                System.out.println("Test passed");
+            } else
+                System.out.println("Test failed");
+
+            WebElement element = driver.findElement(By.cssSelector("a.btn-link"));
+
+            System.out.println(element.getAccessibleName());
+
+            driver.findElement(By.linkText("Features")).click();
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        } finally {
+            driver.quit();
         }
-        else System.out.println("Test failed");
-
-        driver.quit();
     }
 }
