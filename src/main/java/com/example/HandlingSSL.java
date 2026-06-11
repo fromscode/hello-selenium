677a12f46cb44d5394dc20893983a95d3e4b2a3f
fromscode <chakrabortyshubham00@gmail.com>
2026-06-11 09:10:02 +0530

    Handling SSL

---
+++ src/main/java/com/example/HandlingSSL.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class HandlingSSL {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+        options.addArguments("--headless=new");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://expired.badssl.com/");
+
+        if (driver.getTitle().equals("expired.badssl.com")) {
+            System.out.println("Test passed");
+        }
+        else System.out.println("Test failed");
+
+        driver.quit();
+    }
+}
